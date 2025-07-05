19a88e9a9926bf1f081b353198d32e41aad82288
nguyenketri <nguyenketri1803@gmail.com>
2025-07-05 23:33:03 +0700

    style for list singer

--- src/pages/Singers/Singers.js
+++ src/pages/Singers/Singers.js
@@ -1,16 +1,22 @@
 export const SingersPage = () => {
   const singers = [
-  { id: 1, name: "Sơn Tùng MTP", image: "https://via.placeholder.com/150x150?text=Sơn+Tùng" },
-  { id: 2, name: "Mỹ Tâm", image: "https://via.placeholder.com/150x150?text=Mỹ+Tâm" },
-  { id: 3, name: "Đen Vâu", image: "https://via.placeholder.com/150x150?text=Đen+Vâu" },
-  { id: 4, name: "Hòa Minzy", image: "https://via.placeholder.com/150x150?text=Hòa+Minzy" },
-  { id: 5, name: "Jack", image: "https://via.placeholder.com/150x150?text=Jack" }
+  { id: 1, name: "Sơn Tùng MTP",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 2, name: "Mỹ Tâm",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 3, name: "Đen Vâu",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 4, name: "Hòa Minzy",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 5, name: "Jack",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 6, name: "Sơn Tùng MTP",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 7, name: "Mỹ Tâm",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 8, name: "Đen Vâu",title: "ABC", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 9, name: "Hòa Minzy", title: "ABC",image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" },
+  { id: 10, name: "Jack", title: "ABC",image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS18yWzkfTbO6_49aWm3-bvLzzKBaedfug8cw&s" }
+
 ];
   return(
     // <>
     //   <h1 className="text-[32px] text-[dark] font-[700] h-[3000px] bg-white">Singers Page</h1>
     // </>
-    <div className="grid grid-cols-2 gap-4 p-4">
+    <div className="grid grid-cols-5 gap-5 p-4">
     {singers.map(s => (
       <div key={s.id} className="bg-white rounded-lg shadow p-4 text-center">
         <img 
@@ -18,7 +24,10 @@ export const SingersPage = () => {
           alt={s.name} 
           className="w-32 h-32 mx-auto rounded-full object-cover"
         />
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
         <h3 className="mt-2 font-semibold text-lg">{s.name}</h3>
+        <p>{s.title}</p>
+        </div>
       </div>
     ))}
   </div>
