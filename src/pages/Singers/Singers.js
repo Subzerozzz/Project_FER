7ceb27cd13964427b00f9e5a02593d35cea56cbb
nguyenketri <nguyenketri1803@gmail.com>
2025-07-06 00:56:43 +0700

    finish render list singers

--- src/pages/Singers/Singers.js
+++ src/pages/Singers/Singers.js
@@ -1,23 +1,20 @@
 export const SingersPage = () => {
-  const singers = [
-  { id: 1, name: "Sơn Tùng MTP",title: "ABC", image: "/demo/singer/son_tung.jpg" },
-  { id: 2, name: "Nal",title: "ABC", image: "/demo/singer/nal.jpg" },
-  { id: 3, name: "Tuấn Hưng",title: "ABC", image: "/demo/singer/tuan_hung.jpg" },
-  { id: 4, name: "Jimmii Nguyễn",title: "ABC", image: "/demo/singer/jimmii-nguyen.jpg" },
-  { id: 5, name: "Big Daddy",title: "ABC", image: "/demo/singer/big_daddy.jpg" },
-  { id: 6, name: "Soobin Hoàng Sơn",title: "ABC", image: "/demo/singer/soobin.jpg" },
-  { id: 7, name: "Mỹ Tâm",title: "ABC", image: "/demo/singer/my_tam.jpg" },
-  { id: 8, name: "Đen Vâu",title: "ABC", image: "/demo/singer/den_vau.jpg" },
-  { id: 9, name: "Hòa Minzy", title: "ABC",image: "/demo/singer/hoa_minzy.jpg" },
-  { id: 10, name: "Min", title: "ABC",image: "/demo/singer/min.jpg" },
-  { id: 11, name: "Du Thiên",title: "ABC", image: "/demo/singer/du_thien.jpg" },
-  { id: 12, name: "Tùng Dương",title: "ABC", image: "/demo/singer/tung_duong.jpg" },
-  { id: 13, name: "Anh Tú",title: "ABC", image: "/demo/singer/anh_tu.jpg" },
-  { id: 14, name: "Phan Mạnh Quỳnh",title: "ABC", image: "/demo/singer/phan_manh_quynh.jpg" },
-  { id: 15, name: "Quang Hung", title: "ABC",image: "/demo/singer/quang_hung.jpg" },
-  
-
-
+ const singers = [
+  { id: 1, name: "Sơn Tùng MTP", title: "Ca sĩ, nhạc sĩ, rapper", image: "/demo/singer/son_tung.jpg" },
+  { id: 2, name: "Nal", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/nal.jpg" },
+  { id: 3, name: "Tuấn Hưng", title: "Ca sĩ", image: "/demo/singer/tuan_hung.jpg" },
+  { id: 4, name: "Jimmii Nguyễn", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/jimmii-nguyen.jpg" },
+  { id: 5, name: "Big Daddy", title: "Rapper, ca sĩ", image: "/demo/singer/big_daddy.jpg" },
+  { id: 6, name: "Soobin Hoàng Sơn", title: "Ca sĩ, rapper, nhạc sĩ", image: "/demo/singer/soobin.jpg" },
+  { id: 7, name: "Mỹ Tâm", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/my_tam.jpg" },
+  { id: 8, name: "Đen Vâu", title: "Rapper, nhạc sĩ", image: "/demo/singer/den_vau.jpg" },
+  { id: 9, name: "Hòa Minzy", title: "Ca sĩ", image: "/demo/singer/hoa_minzy.jpg" },
+  { id: 10, name: "Min", title: "Ca sĩ, vũ công", image: "/demo/singer/min.jpg" },
+  { id: 11, name: "Du Thiên", title: "Ca sĩ", image: "/demo/singer/du_thien.jpg" },
+  { id: 12, name: "Tùng Dương", title: "Ca sĩ", image: "/demo/singer/tung_duong.jpg" },
+  { id: 13, name: "Anh Tú", title: "Ca sĩ", image: "/demo/singer/anh_tu.jpg" },
+  { id: 14, name: "Phan Mạnh Quỳnh", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/phan_manh_quynh.jpg" },
+  { id: 15, name: "Quang Hùng", title: "Ca sĩ ", image: "/demo/singer/quang_hung.jpg" }
 ];
   return(
     // <>
@@ -25,7 +22,7 @@ export const SingersPage = () => {
     // </>
     <>
     <div className="text-white ml-5 text-3xl font-medium">List Singers</div>
-     <div className="grid grid-cols-5 gap-1d p-4 ">
+     <div className="grid grid-cols-5  p-5 ">
         {singers.map(s => (
         <div key={s.id} className="flex flex-col ">
           <img 
@@ -36,7 +33,7 @@ export const SingersPage = () => {
           />
           <div style={{ display: "flex", flexDirection: "column", alignItems: "flex-start",marginBottom:'15px' }}>
            <h3 className="mt-2 font-semibold text-lg text-white">{s.name}</h3>
-           <p className="text-gray-500 text-[12px]">{s.title}</p>
+           <p className="text-gray-500 text-[12px] truncate w-[150px]">{s.title}</p>
           </div>
         </div>
       ))}
