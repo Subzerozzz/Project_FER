a9c59c3fef8d549fb8e271fb5ab4622809b59913
nguyenketri <nguyenketri1803@gmail.com>
2025-07-06 16:50:51 +0700

    add file music_data.json

--- src/pages/Singers/Singers.js
+++ src/pages/Singers/Singers.js
@@ -1,27 +1,10 @@
 import { useState } from "react";
 import { HeaderSearch } from "../../components/common/HeaderSearch/HeaderSearch";
-
+import musicData from '../../data/music_data.json'
 export const SingersPage = () => {
   const [keyword, setKeyword] = useState("");
-
-  const singers = [
-    { id: 1, name: "Sơn Tùng MTP", title: "Ca sĩ, nhạc sĩ, rapper", image: "/demo/singer/son_tung.jpg" },
-    { id: 2, name: "Nal", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/nal.jpg" },
-    { id: 3, name: "Tuấn Hưng", title: "Ca sĩ", image: "/demo/singer/tuan_hung.jpg" },
-    { id: 4, name: "Jimmii Nguyễn", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/jimmii-nguyen.jpg" },
-    { id: 5, name: "Big Daddy", title: "Rapper, ca sĩ", image: "/demo/singer/big_daddy.jpg" },
-    { id: 6, name: "Soobin Hoàng Sơn", title: "Ca sĩ, rapper, nhạc sĩ", image: "/demo/singer/soobin.jpg" },
-    { id: 7, name: "Mỹ Tâm", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/my_tam.jpg" },
-    { id: 8, name: "Đen Vâu", title: "Rapper, nhạc sĩ", image: "/demo/singer/den_vau.jpg" },
-    { id: 9, name: "Hòa Minzy", title: "Ca sĩ", image: "/demo/singer/hoa_minzy.jpg" },
-    { id: 10, name: "Min", title: "Ca sĩ, vũ công", image: "/demo/singer/min.jpg" },
-    { id: 11, name: "Du Thiên", title: "Ca sĩ", image: "/demo/singer/du_thien.jpg" },
-    { id: 12, name: "Tùng Dương", title: "Ca sĩ", image: "/demo/singer/tung_duong.jpg" },
-    { id: 13, name: "Anh Tú", title: "Ca sĩ", image: "/demo/singer/anh_tu.jpg" },
-    { id: 14, name: "Phan Mạnh Quỳnh", title: "Ca sĩ, nhạc sĩ", image: "/demo/singer/phan_manh_quynh.jpg" },
-    { id: 15, name: "Quang Hùng", title: "Ca sĩ ", image: "/demo/singer/quang_hung.jpg" }
-  ];
-
+ const singers = musicData.singers
+  
   // Lọc danh sách dựa trên keyword
   const filteredSingers = singers.filter(s =>
     s.name.toLowerCase().includes(keyword.toLowerCase())
