3a431d26b7af399e59df0c8b9bf461ef57fb8c7f
duynq112004 <nguyenduyntn112004@gmail.com>
2025-07-06 16:12:36 +0700

    Done SongDeatail

--- src/pages/Song/SongDetail.js
+++ src/pages/Song/SongDetail.js
@@ -1,58 +1,30 @@
 import { CardInfo } from "../../components/common/Card/CardInfo";
-import { SongItem2 } from "../../components/common/Song/SongItem2";
 import { Title } from "../../components/common/Title/Title";
+import { Section3 } from "./Section3";
 
 export const SongDetail = () => {
-  const data = [
-    {
-      image: "/demo/home/image-1.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/home/image-1.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/home/image-1.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/home/image-1.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-    {
-      image: "/demo/home/image-1.png",
-      title: "Cô Phòng",
-      singer: "Hồ Quang Hiếu, Huỳnh Văn",
-      time: "4:32",
-    },
-  ];
   return (
     <>
       <div>
+        {/* CardInfo  */}
         <CardInfo
           image="/demo/home/image-8.png"
           title="Cô Phòng"
           description="Hồ Quang Hiếu, Huỳnh Văn"
         />
-        <Title text="Lời Bài Hát" />
-        {/* List Cùng Danh Mục  */}
-        <div>
-          <Title text="Bài Hát Cùng Danh Mục" />
-          <div className="grid grid-cols-1 gap-[10px]">
-            {data.map((item, index) => (
-              <SongItem2 key={index} {...item} />
-            ))}
+        {/* Lời Bài Hát  */}
+        <div className="">
+          <Title text="Lời Bài Hát" />
+          <div className="bg-[#212121] p-[20px] text-[#FFFFFF] rounded-[15px] text-[14px] font-[500]">
+            Verse: Níu ngàn lời cũng không ngăn được Bàn chân bước đi không báo
+            trước Có những điều cất riêng trong lòng Giờ bốn vách ngăn cùng cô
+            phòng Trốn chạy rồi hàn huyên với men Cứ thế kết thân cùng ánh đèn
+            Lối mòn ngày nào trên phố quen Vẫn đó dáng hình ngày người đến
           </div>
         </div>
+
+        {/* List Cùng Danh Mục  */}
+        <Section3 />
       </div>
     </>
   );
