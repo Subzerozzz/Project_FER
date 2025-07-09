e7a1a4d8a25cebca445f3e7e809a1f1dd4113f43
duynq112004 <nguyenduyntn112004@gmail.com>
2025-07-09 10:33:23 +0700

    Draft of HeaderSearch

--- src/pages/Singers/Section1.js
+++ src/pages/Singers/Section1.js
@@ -1,89 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { CardItem } from "../../components/common/Card/CardItem";
 import { Title } from "../../components/common/Title/Title";
 
 export const Section1 = () => {
-  const data = [
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-    {
-      title: "Sơn Tùng M-TP",
-      image: "/demo/home/image-7.png",
-      description:
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-      link: "#",
-    },
-  ];
+  const [dataFinal, setDataFinal] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:9999/singers").then((res) => {
+      const singerArrayRaw = res.data;
+      if (singerArrayRaw) {
+        const singerArray = singerArrayRaw.map((item) => {
+          return { ...item, link: `/singers/${item.id}` };
+        });
+        setDataFinal(singerArray);
+      }
+    });
+  }, []);
   return (
     <>
       <div className="">
         <Title text="Danh Sách Ca Sĩ" />
 
         <div className="grid grid-cols-5 gap-[20px]">
-          {/* Item  */}
-          {data.map((item, index) => (
-            <CardItem key={index} {...item} />
-          ))}
+          {dataFinal.length > 0 ? (
+            <>
+              {dataFinal.map((item, index) => (
+                <CardItem key={index} {...item} />
+              ))}
+            </>
+          ) : (
+            <>
+              <Title text="Không Có Ca Sĩ Nào" />
+            </>
+          )}
         </div>
       </div>
     </>
