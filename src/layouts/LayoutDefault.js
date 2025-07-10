535221833d75061b6e541049ec316ce805d0ac2d
nguyenketri <nguyenketri1803@gmail.com>
2025-07-10 23:38:42 +0700

    singer page

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,30 +1,63 @@
+// import { Outlet } from "react-router-dom"
+// import { HeaderSearch } from "../components/common/HeaderSearch/HeaderSearch"
+// import { Play } from "../components/common/Play/Play"
+// import { Sider } from "../components/common/Sider/Sider"
+
+// export const LayoutDefault = () => {
+//   return(
+//     <>
+//       <div className=''>
+//         <div className='container mx-[auto]'>
+//           <div className="flex gap-[20px]">
+//             <div className="sidebar w-[280px]">
+//             <Sider />
+//             </div>
+//             <div className="flex-1 mt-[20px]">
+//               <HeaderSearch/>
+//               <main className="mb-[100px]">
+//                 <Outlet/>
+//               </main>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <div>
+//         <Play/>
+//       </div> 
+      
+//     </>
+//   )
+// }
+
 import { Outlet } from "react-router-dom"
 import { HeaderSearch } from "../components/common/HeaderSearch/HeaderSearch"
 import { Play } from "../components/common/Play/Play"
 import { Sider } from "../components/common/Sider/Sider"
+import { useState } from "react"
 
 export const LayoutDefault = () => {
+  const [keyword, setKeyword] = useState("");
+
   return(
     <>
       <div className=''>
         <div className='container mx-[auto]'>
           <div className="flex gap-[20px]">
             <div className="sidebar w-[280px]">
-            <Sider />
+              <Sider />
             </div>
             <div className="flex-1 mt-[20px]">
-              <HeaderSearch/>
+              <HeaderSearch onSearch={(kw) => setKeyword(kw)} />
               <main className="mb-[100px]">
-                <Outlet/>
+                <Outlet context={{ keyword }} />
               </main>
             </div>
           </div>
         </div>
       </div>
       <div>
-        <Play/>
-      </div> 
-      
+        <Play />
+      </div>
     </>
   )
 }
