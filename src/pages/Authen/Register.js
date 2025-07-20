04bec8ca61897fa82dba0260bc31a85d6a9c69f6
duynq112004 <nguyenduyntn112004@gmail.com>
2025-07-20 11:11:37 +0700

    Tính năng đăng ký

--- src/pages/Authen/Register.js
+++ src/pages/Authen/Register.js
@@ -1,6 +1,71 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Title } from "../../components/common/Title/Title";
 
 export const RegisterPage = () => {
+  const [accountArray, setAccountArray] = useState([]);
+  const [errorEmail, setErrorEmail] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:9999/accounts")
+      .then(({ data }) => setAccountArray(data));
+  }, []);
+
+  const generateRandomID = (length) => {
+    const chars =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      result += chars[randomIndex];
+    }
+    return result;
+  };
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+    const role = "customer";
+    const createdAt = new Date();
+    const signUp = new Date();
+    const id = generateRandomID(6);
+
+    // Validate data email
+    const existEmail = accountArray.find((item) => item.email == email);
+    let newEmailError = "";
+    if (existEmail) {
+      newEmailError = "Email đã tồn tại trong hệ thống!";
+      setErrorEmail(newEmailError);
+      return;
+    } else {
+      newEmailError = "";
+      setErrorEmail(newEmailError);
+    }
+
+    //Tao 1 doi tuong moi
+
+    const newAccount = {
+      id: id,
+      fullName: name,
+      email: email,
+      password: password,
+      role: role,
+      username: username,
+      createdAt: createdAt,
+      signUpAt: signUp,
+    };
+
+    axios
+      .post("http://localhost:9999/accounts", newAccount)
+      .then(({ item }) => {
+        alert("Đăng ký tài khoản thành công !");
+      });
+  };
+
   return (
     <>
       <div className="mt-[60px] w-[500px] mx-auto">
@@ -8,7 +73,7 @@ export const RegisterPage = () => {
           <Title text="Đăng Ký Tài Khoản" />
         </div>
 
-        <form>
+        <form onSubmit={handleRegister}>
           {/* Họ Và Tên  */}
           <div className="mb-[15px]">
             <label
@@ -44,6 +109,31 @@ export const RegisterPage = () => {
               className="h-[50px] w-full  bg-white rounded-[6px] px-[16px] font-[600] text-[14px] outline-none"
               required={true}
             />
+            {errorEmail != "" ? (
+              <div style={{ color: "red", fontSize: "0.875rem" }}>
+                {errorEmail}
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
+          {/* User name  */}
+          <div className="mb-[15px]">
+            <label
+              className="block mg-[5px] font-[600] text-[14px]"
+              htmlFor="username"
+            >
+              <span className="text-white">Username</span>
+              <span className="text-red-500 ml-[5px]">*</span>
+            </label>
+            <input
+              type="text"
+              placeholder="levana@gmail.com"
+              id="username"
+              name="username"
+              className="h-[50px] w-full  bg-white rounded-[6px] px-[16px] font-[600] text-[14px] outline-none"
+              required={true}
+            />
           </div>
           {/* Password  */}
           <div className="mb-[15px]">
@@ -55,19 +145,19 @@ export const RegisterPage = () => {
               <span className="text-red-500 ml-[5px]">*</span>
             </label>
             <input
-              type="text"
+              type="password"
               id="password"
               name="password"
               className="h-[50px] w-full  bg-white rounded-[6px] px-[16px] font-[600] text-[14px] outline-none"
               required={true}
             />
           </div>
-          {/* Đăng nhập  */}
+          {/* Đăng ký  */}
           <button
             type="submit"
             className="h-[50px] w-full  bg-[#00ADEF] rounded-[6px] px-[16px] font-[700] text-[16px] outline-none text-[#FFFFFF]"
           >
-            Đăng Nhập
+            Đăng Ký
           </button>
         </form>
       </div>
